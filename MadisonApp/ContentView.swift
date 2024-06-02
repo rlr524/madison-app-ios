4bec3291123f48119e2627474fecf37b385663e8
Rob Ranf <rob@emiyaconsulting.com>
2024-06-02 12:08:37 -0700

    Initial Commit

---
+++ MadisonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MadisonApp
+//
+//  Created by Rob Ranf on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MadisonApp/MadisonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadisonAppApp.swift
+//  MadisonApp
+//
+//  Created by Rob Ranf on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MadisonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
